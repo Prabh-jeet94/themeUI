2c343a3b8adf7faa4f3ac9cd041ed2de7e15ca48
Prabhjeet Kaur <Prabhjeetkaur@Infrrd.ai>
2019-01-08 12:56:42 +0530

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
